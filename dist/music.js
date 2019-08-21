93e8f1f3a9f2d4f2916e08adb1f0a0cc04b2656c
yonger0 <why2002002002@163.com>
2019-08-21 18:52:13 +0800

    Site updated: 2019-08-21 18:52:09

--- dist/music.js
+++ dist/music.js
@@ -1,25 +1,37 @@
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    fixed: true,
-    autoplay: false,
-    audio: [
-      {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
-      },
-      {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
-      },
-      {
-        name: '风筝误',
-        artist: '刘珂矣',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
-      }
+    container: document.getElementById('player'),
+    mini: true,
+    audio: [{
+        name: '入世',
+        artist: '王者荣耀',
+        url: 'https://i.y.qq.com/v8/playsong.html?platform=11&appshare=android_qq&appversion=9020007&hosteuin=ow4AowSiNeEion**&songmid=003PN4Sq2rYmQO&type=0&appsongtype=1&_wv=1&source=qq&ADTAG=qfshare',
+        cover: 'cover.jpg',
+
+    }
+        {
+        name: '归隐',
+        artist: '王者荣耀',
+        url: 'https://i.y.qq.com/v8/playsong.html?platform=11&appshare=android_qq&appversion=9020007&hosteuin=ow4AowSiNeEion**&songmid=000RYe1h2XKc2A&type=0&appsongtype=1&_wv=1&source=qq&ADTAG=qfshare',
+        cover: 'cover2.jpg',
+        lrc: 'lrc2.lrc',
+        theme: '#46718b'
+        }
+           {
+
+
+        name: '7years',
+        artist: 'Lukas Graham',
+        url: 'https://i.y.qq.com/v8/playsong.html?platform=11&appshare=android_qq&appversion=9020007&hosteuin=ow4AowSiNeEion**&songmid=000IvjwO1NYczE&type=0&appsongtype=1&_wv=1&source=qq&ADTAG=qfshare',
+        cover: 'ove.jpg',
+        lrc: 'lrc2.lrc',
+        theme: '#467158'
+
+        }
+
+        
+
+
+
+
     ]
 });
